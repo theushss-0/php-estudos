b0efb79033b704734a58f052417d7b3b812e5b3e
Matheus Silva <theushss.dev@gmail.com>
2026-04-26 21:40:09 -0300

    corrigindo ex_combinacao_v2.php

--- ex_combinacao_v2.php
+++ ex_combinacao_v2.php
@@ -3,8 +3,10 @@
 $nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 for($i=0; $i< count($nums); $i++){
-    for($j=$i+1; $j<count($nums); $j++){
-        for($k=$j+1; $k<count($nums); $k++){
+    for($j=0; $j<count($nums); $j++){
+        if($i == $j) continue;
+        for($k=0; $k<count($nums); $k++){
+            if($i == $k or $j == $k) continue;
             echo "(" . $nums[$i] . "," . $nums[$j] . "," .  $nums[$k] . ")  "; 
         }
         echo "\n";
