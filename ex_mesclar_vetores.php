01a16b34db8288b8a5c2fd2b135f38b14cf3e79d
Matheus Silva <theushss.dev@gmail.com>
2026-04-28 16:08:45 -0300

    alterações nos exercicios

--- ex_mesclar_vetores.php
+++ ex_mesclar_vetores.php
@@ -1,6 +1,6 @@
 <?php
 
-$vetor1 = [1,2,3];
+$vetor1 = [1,2,6];
 $vetor2 = [4,5,6];
 
 $vetor3 = [...$vetor1,...$vetor2];
@@ -24,22 +24,42 @@ function ordenar_vetor($vet){
     return $vet;
 }
 
-function ordenar_vetores($vet1, $vet2){
+function ordenar_vetores($vet1, $vet2, $tam1){
     $vet3 = [];
-    for($i=0; $i<count($vet1); $i++){
-        for($j=0;$j<count($vet2);$j++){
-            if($vet1[$i] < $vet2[$j]){
-                $vet3[] = $vet1[$i];
-            }else{
-                $vet3[] = $vet2[$j];
-            }
+    $i = 0;
+    $j = 0;
+    while($i < $tam1){
+        if($vet1[$i] < $vet2[$j]){
+            $vet3[] = $vet1[$i];
+            unset($vet1[$i]);
+            $i++;
+            $j++;
+        }else{
+            $vet3 = $vet2[$j];
+            unset($vet2[$j]);
+            $j++;
+            
         }
     }
+    $i = 0;
+    while($i < count($vet1)){
+        $vet3[] = $vet1[$i];
+        $i++;    
+    }
+
+    $j=0;
+    while($j < count($vet2)){
+        $vet3[] = $vet2[$j];
+        $j++;
+    }
+    
+    var_dump($vet2);
+
     return $vet3;
 }
 
 $vet4 = ordenar_vetor($vetor3);
-$vet3 = ordenar_vetores($vetor1, $vetor2);
+$vet3 = ordenar_vetores($vetor1, $vetor2, count($vetor1));
 
 
 for($i=0;$i<count($vet3); $i++){
