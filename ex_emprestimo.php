aba0c34bf48b22b0700dff33fb99d8a0ab474d0b
Matheus Silva <theushss.dev@gmail.com>
2026-04-29 22:08:19 -0300

    adicionando comentarios ao ex_emprestimo

--- ex_emprestimo.php
+++ ex_emprestimo.php
@@ -23,14 +23,17 @@ while(True){
     $vlr_max_prestacoes = $salario * 0.3;
     $aux_vlr_prestacoes = $valor_emprestimo/$nr_prestacoes;
 
+    // verifica se o valor das prestações é maior que o permitido
     if($aux_vlr_prestacoes > $vlr_max_prestacoes){
         echo "\n### Valor das prestações muito alto!!(Valor das prestações: R$ ${aux_vlr_prestacoes} | Limite permitido: até R$ ${vlr_max_prestacoes})\n";
         continue;
     }
 
+    // verifica o percentual de cada parcela
     $percentual_prestacao = (1/$nr_prestacoes);
     
 
+    // recebe o valor de cada parcela em um array
     for($i=0; $i<$nr_prestacoes; $i++){
         $prestacoes[] = ($valor_emprestimo*$percentual_prestacao);
     }
