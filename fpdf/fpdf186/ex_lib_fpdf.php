79440475e4777e812bfc7ac1ac1f6b7702137968
Matheus Silva <theushss.dev@gmail.com>
2026-04-30 16:57:54 -0300

    atualizando

--- fpdf/fpdf186/ex_lib_fpdf.php
+++ fpdf/fpdf186/ex_lib_fpdf.php
@@ -8,10 +8,13 @@ $produtos = [[1,"Produto 1"],[1,"Produto 2"],[1,"Produto 3"]];
 
 $pdf = new PDF();
 $pdf->AddPage();
-$pdf->SetFont("Arial", "B", 32);
+$pdf->SetFont("Arial", "B", 9);
 $pdf->Image("../imagens/logo.jpg",10,10,49);
 $pdf->Cell(49,48,"  ",1);
-$pdf->Cell(141,48,"Hello World",1);
+$pdf->Cell(141,5,"Hello World",1, $ln=1,$align="C");
+$pdf->SetFont("Arial", "B", 32);
+$pdf->Cell(49,45,"  ",0);
+$pdf->Cell(141,43.5,"Hello World",1,);
 $pdf->Ln();
 $pdf->SetFont("Arial", "B", 11);
 $pdf->Cell(95,5,$produtos[0][0],1);
