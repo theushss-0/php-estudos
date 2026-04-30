158dba6518ea738d6181b76d3d67b049da08efdb
Matheus Silva <theushss.dev@gmail.com>
2026-04-30 16:16:50 -0300

    Alteração nos programas

--- fpdf/fpdf186/ex_lib_fpdf.php
+++ fpdf/fpdf186/ex_lib_fpdf.php
@@ -4,9 +4,24 @@ class PDF extends FPDF{
 
 }
 
+$produtos = [[1,"Produto 1"],[1,"Produto 2"],[1,"Produto 3"]];
+
 $pdf = new PDF();
 $pdf->AddPage();
 $pdf->SetFont("Arial", "B", 32);
-$pdf->Cell(190,50,"Hello World",1,$ln=2, $align="C");
+$pdf->Image("../imagens/logo.jpg",10,10,49);
+$pdf->Cell(49,48,"  ",1);
+$pdf->Cell(141,48,"Hello World",1);
+$pdf->Ln();
+$pdf->SetFont("Arial", "B", 11);
+$pdf->Cell(95,5,$produtos[0][0],1);
+$pdf->Cell(95,5,$produtos[0][1],1);
+$pdf->Ln();
+$pdf->Cell(95,5,$produtos[1][0],1);
+$pdf->Cell(95,5,$produtos[1][1],1);
+$pdf->Ln();
+$pdf->Cell(95,5,$produtos[2][0],1);
+$pdf->Cell(95,5,$produtos[2][1],1);
+$pdf->Ln();
 $pdf->Output('F', 'teste.pdf');
 
